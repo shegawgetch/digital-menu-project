de54680bd43cceed62ea64c5475318a8fd110915
Shegaw Getie <shegawgetch@gmail.com>
2025-10-18 11:27:45 +0300

    API

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('firt_dfoyename');
+           // $table->string('name');                                                                                                          ename');
             $table->string('email')>unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
