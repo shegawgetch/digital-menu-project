c433d38a19d505532b6bf78b4ef73622ba766cdd
Shegaw Getie <shegawgetch@gmail.com>
2025-12-05 21:53:54 +0300

    Cors issue fexed!!

--- config/cors.php
+++ config/cors.php
@@ -3,9 +3,9 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'], // or your frontend URL: 'https://shegu-digital-menu.onrender.com'
+    'allowed_origins' => ['https://shegu-digital-menu.onrender.com'],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false, // ❌ false for token-based auth
+    'supports_credentials' => false, // token-based auth
 ];
